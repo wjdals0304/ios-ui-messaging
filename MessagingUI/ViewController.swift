e05eeb6963e2ba3a9917c7d2bcde8117f12502f2
jmkim <wjdals0304@gmail.com>
2022-03-26 17:11:15 +0900

    Initial Commit

---
+++ MessagingUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessagingUI
+//
+//  Created by 김정민 on 2022/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
